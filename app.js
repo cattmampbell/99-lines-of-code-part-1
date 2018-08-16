0aca8d70e315dcd9246c2c2d4bf9ea1131f48df5
Matthew Campbell <cattmampbell@gmail.com>
2018-08-15 22:43:44 -0400

    Added content to index.html and app.js files.

---
+++ app.js
@@ -0,0 +1,5 @@
+let myFriends = ["OLIVIA", "CODY", "MEGAN", "EMILY", "TOAST"]; // An array of 'myFriends'.
+
+for(let i = 0; i < myFriends.length; i++) {
+    console.log(myFriends[i]); // Should print: Each String in 'myFriends'.
+};  
